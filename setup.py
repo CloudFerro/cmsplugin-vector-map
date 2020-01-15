16b027463233bf627406060a26f700db11d370c9
Radosław Stępień <rstepien@cloudferro.com>
2020-01-15 13:54:13 +0100

    add a wrapper to enable more css flexibility

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cmsplugin-vector-map",
-    version='1.0.3',
+    version='1.0.4',
     url='https://gitlab.mtechnology.pl/packages/django/cmsplugin-vector-map',
     packages=find_packages(where='src'),
     include_package_data=True,
