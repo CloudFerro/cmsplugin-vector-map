13d75cef3518b58d0c69ff3ec157b5160e5bc792
Radosław Stępień <rstepien@cloudferro.com>
2020-01-27 13:22:29 +0100

    more validation error fixes, version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cmsplugin-vector-map",
-    version='1.0.4',
+    version='1.0.5',
     url='https://gitlab.mtechnology.pl/packages/django/cmsplugin-vector-map',
     packages=find_packages(where='src'),
     include_package_data=True,
